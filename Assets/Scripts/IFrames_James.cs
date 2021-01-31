665b0114690c1c3cbecf0abe7ce15980ed402d8c
james <b9017317@my.shu.ac.uk>
2021-01-31 18:45:48 +0000

    Added 3 scripts
    Added basic movement and i-frames, started combat

---
+++ Assets/Scripts/IFrames_James.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IFrames_James : MonoBehaviour
+{
+    [SerializeField]float I_FRAMES_TIME;
+    private bool isInvincible = false;
+
+
+    private void IFramesOff()    //Called when i-frames are over.
+    {
+        isInvincible = false;
+    }
+
+
+    public void StartIFramesTimer()    //Called by another script when i-frames should start.
+    {
+        isInvincible = true;
+        Invoke(nameof(IFramesOff), I_FRAMES_TIME);
+    }
+
+
+    public bool GetIsInvincible()    //Allows state of invicibility to be fetched externally.
+    {
+        return isInvincible;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement_James.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_James : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+
+    [SerializeField] float BASE_SPEED;
+
+    [SerializeField] KeyCode LEFT_BUTTON;
+
+    [SerializeField] KeyCode RIGHT_BUTTON;
+
+    bool facingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(LEFT_BUTTON))
+        {
+            MoveLeft();
+
+            if (facingRight)
+                FlipPlayer();
+
+            facingRight = false;
+        }
+
+        else if (Input.GetKey(RIGHT_BUTTON))
+        {
+            MoveRight();
+
+            if (!facingRight)
+                FlipPlayer();
+
+            facingRight = true;
+        }
+
+        if (!Input.GetKey(LEFT_BUTTON) && !Input.GetKey(RIGHT_BUTTON) && rb.velocity.x != 0)
+        {
+            ResetVelocity();
+        }
+
+        //Turn the player in the direction they are moving.
+        void FlipPlayer()
+        {
+          transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+
+        //Ran when player inputs to move left
+        void MoveLeft()
+        {
+            rb.velocity = new Vector2(-BASE_SPEED, rb.velocity.y);
+        }
+
+
+        //Ran when player inputs to move left
+        void MoveRight()
+        {
+            rb.velocity = new Vector2(BASE_SPEED, rb.velocity.y);
+        }
+
+        //Ran when the player isnt inputting any horizontal movement.
+        void ResetVelocity()
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+}

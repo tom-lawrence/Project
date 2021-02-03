9a86b422caf39de93feb6e9043680527b629cfe9
james <b9017317@my.shu.ac.uk>
2021-02-03 15:47:17 +0000

    Progress with combat script
    Player can now heavy attack and is vulnerable for the first x amount of time. The player is also locked out of movement during x amount of time of either attack. Other movement scripts will access the lockout timer since it is public.

--- Assets/Scripts/PlayerCombat_James.cs
+++ Assets/Scripts/PlayerCombat_James.cs
@@ -5,30 +5,105 @@ using UnityEngine;
 public class PlayerCombat_James : MonoBehaviour
 {
     [SerializeField] Animator anim;
+    [SerializeField] Transform heavyHitbox;
+    [SerializeField] float heavyHitBoxRadius;
+     
+    [SerializeField] Transform lightHitbox;
+    [SerializeField] float lightHitBoxRadius;
+    [SerializeField] LayerMask enemiesLayer;
 
+    [SerializeField] float lightAttackLockout;
+    [SerializeField] float heavyAttackLockout;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] float heavyAttackVulnerableTime;
 
-    }
+    public float lockoutTimer = 0;
+
+    public bool heavyAttackVulnerable = false;
+
 
     // Update is called once per frame
     void Update()
     {
+        if (lockoutTimer >= 0)
+        {
+            //if it is the first x seconds of the heavy attack, make the player vulnerable.
+            if (lockoutTimer >= heavyAttackLockout - heavyAttackVulnerableTime)
+                heavyAttackVulnerable = true;
+            else
+                heavyAttackVulnerable = false;
+
+
+            lockoutTimer -= Time.deltaTime;
+        }
+        else
+            //If the lockout timer is zero, allow the player to input.
+            PlayerInput();
+
+    }
+
+    //Player input goes here.
+    void PlayerInput()
+    {
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Attack();
+            LightAttack();
         }
 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            HeavyAttack();
+        }
 
     }
 
-
-    void Attack()
+    //Everything that happens when the light attack button is pressed.
+    void LightAttack()
     {
-        anim.SetTrigger("Attack");
+        anim.SetTrigger("LightAttack"); //set trigger parameter to LightAttack
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(lightHitbox.position, lightHitBoxRadius, enemiesLayer);
+
+        lockoutTimer = lightAttackLockout;
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log(enemy.name + " was hit.");
+        }
+
     }
 
-    
+    //Everything that happens when the heavy attack button is pressed.
+    void HeavyAttack()
+    {
+        anim.SetTrigger("HeavyAttack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(heavyHitbox.position, heavyHitBoxRadius, enemiesLayer);
+
+        lockoutTimer = heavyAttackLockout;
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log(enemy.name + " was hit.");
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (lightHitbox == null)
+            return;
+
+        //Show a circle where the light attack AOE is in the Unity editor
+        Gizmos.DrawWireSphere(lightHitbox.position, lightHitBoxRadius);
+
+
+        if (heavyHitbox == null)
+            return;
+
+        //Show a circle where the heavy attack AOE is in the Unity editor
+        Gizmos.DrawWireSphere(heavyHitbox.position, heavyHitBoxRadius);
+    }
+
 }
--- Assets/Scripts/PlayerMovement_James.cs
+++ Assets/Scripts/PlayerMovement_James.cs
@@ -6,6 +6,8 @@ public class PlayerMovement_James : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
 
+    public PlayerCombat_James combatScript;
+
     [SerializeField] float BASE_SPEED;
 
     [SerializeField] KeyCode LEFT_BUTTON;
@@ -14,15 +16,20 @@ public class PlayerMovement_James : MonoBehaviour
 
     bool facingRight = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+        //If the player is locked out.
+        if (combatScript.lockoutTimer <= 0)
+            PlayerInput();
+        else
+            ResetVelocity();
+    }
+
+    //Player input goes here
+    void PlayerInput()
+    {
+
         if (Input.GetKey(LEFT_BUTTON))
         {
             MoveLeft();
@@ -47,30 +54,30 @@ public class PlayerMovement_James : MonoBehaviour
         {
             ResetVelocity();
         }
+    }
 
-        //Turn the player in the direction they are moving.
-        void FlipPlayer()
-        {
-          transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
-        }
+    //Turn the player in the direction they are moving.
+    void FlipPlayer()
+    {
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+    }
 
-        //Ran when player inputs to move left
-        void MoveLeft()
-        {
-            rb.velocity = new Vector2(-BASE_SPEED, rb.velocity.y);
-        }
+    //Ran when player inputs to move left
+    void MoveLeft()
+    {
+        rb.velocity = new Vector2(-BASE_SPEED, rb.velocity.y);
+    }
 
 
-        //Ran when player inputs to move left
-        void MoveRight()
-        {
-            rb.velocity = new Vector2(BASE_SPEED, rb.velocity.y);
-        }
+    //Ran when player inputs to move left
+    void MoveRight()
+    {
+        rb.velocity = new Vector2(BASE_SPEED, rb.velocity.y);
+    }
 
-        //Ran when the player isnt inputting any horizontal movement.
-        void ResetVelocity()
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        }
+    //Ran when the player isnt inputting any horizontal movement.
+    void ResetVelocity()
+    {
+        rb.velocity = new Vector2(0, rb.velocity.y);
     }
 }

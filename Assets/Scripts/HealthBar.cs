5a008bd991d5a036c4b809642f3e691fb77eedc9
Madison Jeffs <b5018817@my.shu.ac.uk>
2021-02-08 04:35:17 +0000

    Health bar script added

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    //For application of health slider decling (POST Sprite APP)
+    public Slider slider;
+   
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+    }
+
+    public void SetHealth (int health)
+    {
+        slider.value = health;
+    }
+
+
+    //following is code for player health to be implemented & tested
+
+
+
+
+
+
+
+    //void Update()
+     //{ if (Input.GetKeyDown(KeyCode.Space))
+            { TakeDamage(20);
+    //}
+
+    //void TakeDamage (int damage)
+    //{ currentHealth -= damage;
+
+
+    //healthBar.SetHealth(CurrentHealth);
+}
+        
+   
+
+   
+

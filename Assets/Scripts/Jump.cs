a61c236c776d138924a6acd693a70dd8afdc1ac5
Tom <b8024844@my.shu.ac.uk>
2021-02-10 16:46:29 +0000

    Jump and fall anim

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -7,10 +7,14 @@ public class Jump : MonoBehaviour
 {
     [SerializeField] private LayerMask platformsLayerMask;
     [SerializeField] private float jumpVelocity = 10f;
+
     private CircleCollider2D circleCollider2d;
     private Rigidbody2D rigidbody2d;
     private bool canDoubleJump;
+
     //Animator m_Animator;
+    [SerializeField] Animator anim;
+
     private void Awake()
     {
         
@@ -24,6 +28,8 @@ public class Jump : MonoBehaviour
         if(IsGrounded())
         {
             canDoubleJump = true;
+            anim.SetTrigger("Landed");
+            //anim for landing trigger called
         }
 
         if (Input.GetKeyDown(KeyCode.W))
@@ -35,42 +41,39 @@ public class Jump : MonoBehaviour
                 rigidbody2d.velocity = Vector2.up * jumpVelocity;
                 
             }
-            else
+            else if (canDoubleJump)
             {
-                if (canDoubleJump)
-                {
-                    
-                    rigidbody2d.velocity = Vector2.up * jumpVelocity;
-                    canDoubleJump = false;
-                }
-
-
-
+                rigidbody2d.velocity = Vector2.up * jumpVelocity;
+                canDoubleJump = false;
             }
-
-            //Send the message to the Animator to activate the trigger parameter named "Jump"
-            //m_Animator.SetTrigger("Jump");
-
+            
         }
+
+        AnimCall();
+        //Send the message to the Animator to activate the trigger parameter named "Jump"
+        //m_Animator.SetTrigger("Jump");
+
+    }
+
+
+    //Checks to see if player is grounded, (Using raycast box collider)
+     private bool IsGrounded()
+     {
+         RaycastHit2D raycastHit2d = Physics2D.BoxCast(circleCollider2d.bounds.center, circleCollider2d.bounds.size, 0f, Vector2.down ,.1f, platformsLayerMask);
+         Debug.Log(raycastHit2d.collider);
+         return raycastHit2d.collider != null;
+     }
+  
+     
+    void AnimCall()
+    {
+
 
 
 
     }
 
-    //Checks to see if player is grounded, (Using raycast box collider)
-     private bool IsGrounded()
-     {
-
-         RaycastHit2D raycastHit2d = Physics2D.BoxCast(circleCollider2d.bounds.center, circleCollider2d.bounds.size, 0f, Vector2.down ,.1f, platformsLayerMask);
-         Debug.Log(raycastHit2d.collider);
-         return raycastHit2d.collider != null;
-        
-
-     }
-  
-            
-         
 
 
-    
+
 }

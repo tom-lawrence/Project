42b203df5861e14258a820643561bab6e48b2af1
Madison Jeffs <b5018817@my.shu.ac.uk>
2021-02-18 08:38:04 +0000

    Added All Scenes(other than options) and finished boss health script etc

---
+++ Assets/Scripts/BossHealth.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BossHealth : MonoBehaviour
+{
+   
+
+    public int maxHealth = 150;
+    public int currentHealth;
+    public HealthBar healthbar;
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+
+    }
+
+
+    void Update()
+    {
+       
+
+    }
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthbar.SetHealth(currentHealth);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Player"))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    void bossDeath()
+    {
+        //anim.play("Boss_Death");
+
+
+
+        Debug.Log("Boss is dead");
+
+        SceneManager.LoadScene("WinState");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
---
+++ Assets/Scripts/WinState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinState : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        void MainMenu ()
+        {
+
+            SceneManager.LoadScene("MainMenu");
+
+        }
+
+
+        void QuitGame()
+        {
+
+            Application.Quit();
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
